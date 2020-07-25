8715cecc75a9d9b9414258b77009cd105b917f1f
joellearcher <joelle.archerdmello@gmail.com>
2020-07-25 23:01:48 +0100

    25.07.20_9

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./css/style_all.css";
 import "./css/home.css";
 import "./css/islands.css";
@@ -11,14 +11,12 @@ import Dice from "./components/rollthedice";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/theislands" component={Islands} />
-        <Route path="/rollthedice" component={Dice} />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/theislands" component={Islands} />
+      <Route path="/rollthedice" component={Dice} />
+    </div>
   );
 }
 
